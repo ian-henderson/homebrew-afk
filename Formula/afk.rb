009166194d091fe2b3bd98daeb1f473e7e8cea8a
Ian Henderson <ianhenderson@protonmail.com>
2021-12-24 11:30:30 -0500

    fixed afk.rb

--- Formula/afk.rb
+++ Formula/afk.rb
@@ -2,8 +2,7 @@ class Afk < Formula
   desc "A command-line program to make your mouse wander. ;)"
   homepage "https://github.com/ian-henderson/afk.rs"
   url "https://github.com/ian-henderson/afk.rs/releases/download/v0.1.10/afk-mac.tar.gz"
-  sha256 "7552580e6eca793e8a6d099afbadc0f1342f1b0f20af5f2be4875e06b319c971
-"
+  sha256 "7552580e6eca793e8a6d099afbadc0f1342f1b0f20af5f2be4875e06b319c971"
   version "0.1.10"
 
   def install
